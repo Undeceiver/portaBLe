f2cfa2a8285e6367e98ccb97c0bb1324c406125f
NSGolova <golova@golova.dev>
2024-07-14 17:03:00 +0100

    Added leaderboards pages

---
+++ Pages/Leaderboard.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using portaBLe;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portaBLe.Pages
+{
+    public class LeaderboardModel : PageModel
+    {
+        private readonly AppContext _context;
+        public Leaderboard Leaderboard { get; set; }
+        public List<Score> Scores { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
+
+        public LeaderboardModel(AppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string id, int currentPage = 1)
+        {
+            Leaderboard = await _context.Leaderboards.FirstOrDefaultAsync(l => l.Id == id);
+
+            if (Leaderboard == null)
+            {
+                return NotFound();
+            }
+
+            int pageSize = 10; // Set the number of items per page
+            CurrentPage = currentPage;
+
+            var totalScores = await _context.Scores.Where(s => s.LeaderboardId == id).CountAsync();
+            TotalPages = (int)System.Math.Ceiling(totalScores / (double)pageSize);
+
+            Scores = await _context.Scores
+                                   .Where(s => s.LeaderboardId == id)
+                                   .Include(s => s.Player)
+                                   .OrderByDescending(s => s.Pp)
+                                   .Skip((currentPage - 1) * pageSize)
+                                   .Take(pageSize)
+                                   .ToListAsync();
+
+            return Page();
+        }
+    }
+}
---
+++ Pages/Leaderboards.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using portaBLe;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace portaBLe.Pages
+{
+    public class LeaderboardsModel : PageModel
+    {
+        private readonly AppContext _context;
+        public List<Leaderboard> Leaderboards { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPages { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public bool SortDescending { get; set; }
+
+        public LeaderboardsModel(AppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnGetAsync(int currentPage = 1)
+        {
+            IQueryable<Leaderboard> leaderboardQuery = _context.Leaderboards;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                leaderboardQuery = leaderboardQuery.Where(l => EF.Functions.Like(l.Name, $"%{SearchString}%"));
+            }
+
+            if (SortDescending)
+            {
+                leaderboardQuery = leaderboardQuery.OrderByDescending(l => l.Stars);
+            }
+            else
+            {
+                leaderboardQuery = leaderboardQuery.OrderBy(l => l.Stars);
+            }
+
+            int pageSize = 10; // Set the number of items per page
+            CurrentPage = currentPage;
+            var totalRecords = await leaderboardQuery.CountAsync();
+            TotalPages = (int)System.Math.Ceiling(totalRecords / (double)pageSize);
+
+            Leaderboards = await leaderboardQuery.Skip((currentPage - 1) * pageSize).Take(pageSize).ToListAsync();
+        }
+    }
+}
